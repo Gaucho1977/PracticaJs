94dd52359573fe37f6be2b150e00c080fc7fb24d
Patagonia1977 <waltergusamaya@gmail.com>
2022-07-04 09:46:17 -0300

    subiendo archivos de ejercicio JS

---
+++ script/script.js
@@ -0,0 +1,55 @@
+var c = "c"
+var a1 = "a"
+var s = "s"
+var a2 = "a"
+
+var vidas =3 
+
+var letra = prompt("ingrese la letra a adivinar")
+
+if( letra == c ){
+    alert( " Acertaste .. Excelente")
+
+}else{
+    alert ("has Perdido una vida")
+    vidas--
+}
+
+
+
+var letra = prompt("ingrese la letra a adivinar")
+
+if( letra == a1 ){
+    alert( " Acertaste .. Excelente")
+
+}else{
+    alert ("has Perdido una vida")
+    vidas--
+}
+
+
+
+var letra = prompt("ingrese la letra a adivinar")
+
+if( letra == s ){
+    alert( " Acertaste .. Excelente")
+
+}else{
+    alert ("has Perdido una vida")
+    vidas--
+}
+
+
+
+
+var letra = prompt("ingrese la letra a adivinar")
+
+if( letra == a2 ){
+    alert( " Acertaste .. Excelente")
+
+}else{
+    alert ("has Perdido una vida")
+    
+}
+console.log ("El total de vidas es :"  + vidas)
+alert ("el total de vidas es :" + vidas)
